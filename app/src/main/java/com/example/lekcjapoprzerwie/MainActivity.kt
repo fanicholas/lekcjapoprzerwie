592c843ac2b1a53609a42f96b60381193939d6a2
student <nicholas.fajer123@gmail.com>
2022-01-13 13:19:57 +0100

    pierwsze na 3 zrobione

--- app/src/main/java/com/example/lekcjapoprzerwie/MainActivity.kt
+++ app/src/main/java/com/example/lekcjapoprzerwie/MainActivity.kt
@@ -2,10 +2,62 @@ package com.example.lekcjapoprzerwie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
+        var x = 0;
+        findViewById<Button>(R.id.button).setOnClickListener {
+            if(x<4){
+                x+=1;
+                if(x==1)
+                {
+                    findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.jeden)
+                }
+                if(x==2)
+                {
+                    findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.dwa)
+                }
+                if(x==3)
+                {
+                    findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.trzy)
+                }
+                if(x==4)
+                {
+                    findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.cztery)
+                }
+            }
+            else{
+                x=4;
+            }
+        }
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            if(x != 0){
+                x-=1;
+                if(x==1)
+                {
+                    findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.jeden)
+                }
+                if(x==2)
+                {
+                    findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.dwa)
+                }
+                if(x==3)
+                {
+                    findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.trzy)
+                }
+                if(x==4)
+                {
+                    findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.cztery)
+                }
+            }
+            else{
+                x=0;
+            }
+        }
+
+
+    }}
